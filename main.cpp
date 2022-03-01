1509650ce4db6b32131ff55e9ebfdfd160de0654
Igor Lorenz <lorenz.igor@gmail.com>
2022-03-01 18:35:51 -0300

    new repo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <thread>
+
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
-    const int a = 500;
-    mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_VIRTUALDESK,
-                a,
-                a,
-                0,
-                GetMessageExtraInfo());
+    ShowWindow(GetConsoleWindow(), 0);
+    //std::cout << "Hello, World!" << std::endl;
+    int x, y  = 0;
+    bool isPaused = false;
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        isPaused = (GetAsyncKeyState(VK_HOME)) != 0;
+        if(GetAsyncKeyState(VK_END)){
+            break;
+        }
+        if(GetAsyncKeyState(VK_LBUTTON)) {
+            if (!isPaused) {
+                mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_VIRTUALDESK,
+                            x,
+                            y,
+                            0,
+                            GetMessageExtraInfo());
 
-
-    std::cin.get();
+                y += 150;
+            }
+        }
+    }
 
     return 0;
 }
